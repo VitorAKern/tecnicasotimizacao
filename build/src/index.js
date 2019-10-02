54edfea0ce33478584c93a52a149ce424b59aeaf
VitorKern <vitor_kern@yahoo.com.br>
2019-10-02 19:31:49 -0300

    a

---
+++ build/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Try npm run check/fix!");
+const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const trailing = 'Semicolon';
+const why = 'am I tabbed?';
+function doSomeStuff(withThis, andThat, andThose) {
+    //function on one line
+    alert("112#!23")
+}
+exports.doSomeStuff = doSomeStuff;
+// TODO: more examples
+//# sourceMappingURL=index.js.map
